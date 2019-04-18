71e9ff0e6f42eb185176d489f983de3dbb10a072
griffinGC <griffinDouble@gmail.com>
2019-04-18 20:35:46 +0900

    stop_plugin

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -3,18 +3,18 @@ var router = express.Router();
 const guestModel = require('../schemas/createGuest')
 
 router.get('/',function(req, res, error){
-    if(!req.body.id){
-        res.json({state: 1, msg : "username is empty"});
-    }
-    if(!req.body.pwd){
-        res.json({state:1, msg : "password is empty"})
-    }
+    // if(!req.body.id){
+    //     res.json({state: 1, msg : "username is empty"});
+    // }
+    // if(!req.body.pwd){
+    //     res.json({state:1, msg : "password is empty"})
+    // }
 
-    guestModel.find({guestId : req.body.id }, function(err,guestModel){
-        if(err) return res.status(500).json({error:err});
-        res.json({state:0, msg: "find guest success"});
-    });
-    // res.json({state:0});
+    // guestModel.find({guestId : req.body.id }, function(err,guestModel){
+    //     if(err) return res.status(500).json({error:err});
+    //     res.json({state:0, msg: "find guest success"});
+    // });
+    res.json({state:0});
 
     
 })

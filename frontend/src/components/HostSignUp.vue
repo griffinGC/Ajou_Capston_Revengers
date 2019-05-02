e7e543cdd7a045acdff0b335c7d69af6628aac84
LIJIPU <765149929@qq.com>
2019-05-02 14:01:37 +0900

    installed vietify ui

--- frontend/src/components/HostSignUp.vue
+++ frontend/src/components/HostSignUp.vue
@@ -40,7 +40,90 @@ export default {
   }
 };
 </script>
-<style>
-</style>
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+  /*background: #487eb0;*/
+  background: white;
+}
+.signup-form {
+  width: 40%;
+  padding: 30%;
+  text-align: center;
+  position: absolute;
+  background: url(../assets/bg.png);
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  overflow: hidden;
+}
+
+.signup-form logo {
+  width: auto;
+  height: 40px;
+}
+
+.signup-form h1 {
+  margin-top: 50px;
+  font-family: "Permanent Marker", cursive;
+  color: #fff;
+  font-size: 40px;
+}
+
+.signup-form input {
+  font-family: "Montserrat", sans-serif;
+  display: block;
+  text-align: center;
+  width: 100%;
+  height: 44px;
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+}
+
+.signup-form select {
+  font-family: "Montserrat", sans-serif;
+  display: block;
+  text-align: center;
+  width: 100%;
+  height: 44px;
+  box-sizing: border-box;
+  outline: none;
+  border: none;
+}
+
+.txtb {
+  margin: 20px 0;
+  background: rgba(255, 255, 255, 5);
+  border-radius: 6px;
+}
+
+.signup-btn {
+  margin-top: 60px;
+  margin-bottom: 20px;
+  background: #1e90ff;
+  border-radius: 44px;
+  cursor: pointerl;
+  transition: 0.8s;
+  color: #fff;
+}
+
+.signup-btn :hover {
+  transform: scale(0.96);
+}
+
+.signup-form a {
+  text-decoration: none;
+  color: #fff;
+  font-family: "Montserrat", sans-serif;
+  font-size: 14px;
+  padding: 10px;
+  transform: 0.8s;
+}
+
+.signup-form a:hover {
+  background: rgba(0, 0, 0, 3);
+}
 
 
--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="main-page">
-    <div class="notification">
-      <a>Request List</a><br>
-       <textarea></textarea>
-    </div>
-    <div class="chat">
-      <a>Chat List</a><br>
-      <textarea></textarea>
-    </div>
+<div class="main-page">
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons"
+      rel="stylesheet"
+    >
+  <div class="notification">
+    <a>Request List</a>
+    <br>
+    <textarea></textarea>
   </div>
+  <div class="chat">
+    <a>Chat List</a>
+    <br>
+    <textarea></textarea>
+  </div>
+</div>
 </template>
 
 
 <script>
 export default {
   name: "MainPage",
-  data(){
-    return{
-      id:''
-    }
+  data() {
+    return {
+      id: ""
+    };
   },
-  methods:{
-    
-  }
-
-}
+  methods: {}
+};
 </script>
 
 <style scoped>
-
 </style>

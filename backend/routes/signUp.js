3a5c1e0d871b4bff1f394fa57d84ff12fa5ec07c
griffinGC <griffinDouble@gmail.com>
2019-05-02 20:24:53 +0900

    implement signUp

--- backend/routes/signUp.js
+++ backend/routes/signUp.js
@@ -4,66 +4,49 @@ const guestModel = require('../schemas/createGuest');
 const hostModel = require('../schemas/createHost');
 
 //guestid 확인용, gudstid 가입, hostid 확인, hostid 가입 => 총 4개의 router 지정해야함 
-router.post('/guest',function(req, res,next){
-    if(!req.body.userName){
-        res.json({status: 1, message: 'ID can not be empty!'})
-    }
-    if(!req.body.password){
-            res.json({status: 1, message: 'password can not be empty!'})
-    }
-    // if(!req.body.gender){
-    //     res.json({status: 1, message: 'gender can not be empty!'})
-    // }
-    // if(!req.body.age){
-    //     res.json({status: 1, message: 'age can not be empty!'})
-    // }
-    // if(!req.body.work){
-    //     res.json({status: 1, message: 'work can not be empty!'})
-    // }
-    // if(!req.body.address){
-    //     res.json({status: 1, message: 'address can not be empty!'})
-    // }
-    // if(!req.body.location){
-    //     res.json({status: 1, message: 'location can not be empty!'})
-    // }
-    if(!req.body.phone){
-        res.json({status: 1, message: 'phone can not be empty!'})
-    }
-    if(!req.body.email){
-        res.json({status: 1, message: 'email can not be empty!'})
-    }
+router.get('/:id',function(req, res,next){
+  console.log("start");
+  console.log(req.params.id);
+  res.json({state : 0});
     
-    guestModel.find({userName : req.body.id},function(err,guestModel){
-        console.log(guestModel);
+  });     
+
+router.get('/hostConfirm/:id',function(req, res,next){
+  console.log(req.params.id);
+    hostModel.find({userName : req.params.id},function(err,hostModel){
+      // console.log(req.params.id);
         if(err) {
           return res.json(err);
         };
-        if(guestModel[0]){
-            res.json({state : -1, msg : "ID alreay exists!"})
+        if(hostModel[0]){
+            res.json({state : -1, msg : "host ID alreay exists!"});
         } 
         else {
-            // let registerUser = new guestModel();
-            //   registerUser.userName = req.body.userName;
-            //   registerUser.password = req.body.password;
-            //   registerUser.email = req.body.email;
-            //   registerUser.phone = req.body.phone;
-            //   registerUser.address= req.body.address;
-            //   registerUser.location = req.body.location;
-            //   registerUser.age = req.body.age;
-            //   registerUser.work = req.body.work;
-            //   registerUser.gender = req.body.gender;
-            // registerUser.save(function () {
-            //     if(err){
-            //         res.json({state : -1, msg : err});
-            //     }
-            //   res.json({ status: 0, message: 'guest register success!' })
-            // })
+            res.json({state : 0, msg : "host ID can use"});           
           }
         })
+  });        
         
-        //지금은 find함수 밖으로 분리만 해놓은 상태 => find와 save 동시 불가능 
-        //router 분리 필요 
-        let registerUser = new guestModel();
+router.post('/', function(req, res, next){
+    res.json({state : "testNormal"});
+  });  
+
+router.post('/host',function(req, res,next){ 
+  console.log("test");
+      if(!req.body.userName){
+          res.json({status: -1, message: 'ID can not be empty!'})
+      }
+      if(!req.body.password){
+              res.json({status: -1, message: 'password can not be empty!'})
+      }    
+      if(!req.body.phone){
+          res.json({status: -1, message: 'phone can not be empty!'})
+      }
+      if(!req.body.email){
+          res.json({status: -1, message: 'email can not be empty!'})
+      }
+       console.log(req.body.userName);
+        let registerUser = new hostModel();
         registerUser.userName = req.body.userName;
         registerUser.password = req.body.password;
         registerUser.email = req.body.email;
@@ -77,12 +60,56 @@ router.post('/guest',function(req, res,next){
           if(err){
               res.json({state : -1, msg : "error is occured"});
           }
+        res.json({ status: 0, message: "host register success!" });
+      });
+
+});
+
+
+
+router.get('/guestConfirm/:id',function(req, res,next){
+    guestModel.find({userName : req.params.id},function(err,guestModel){
+        console.log(hostModel);
+        if(err) {
+          return res.json(err);
+        };
+        if(guestModel[0]){
+            res.json({state : -1, msg : "guest ID alreay exists!"})
+        } 
+        else {
+            res.json({state : 0, msg : "guest ID can use"});           
+          }
+        }); 
+});
+
+router.post('/guest',function(req, res,next){
+      if(!req.body.userName){
+        res.json({status: -1, message: 'ID can not be empty!'})
+        }
+        if(!req.body.password){
+            res.json({status: -1, message: 'password can not be empty!'})
+        }    
+        if(!req.body.phone){
+        res.json({status: -1, message: 'phone can not be empty!'})
+        }
+        if(!req.body.email){
+        res.json({status: -1, message: 'email can not be empty!'})
+        }        
+        let registerUser = new guestModel();
+        registerUser.userName = req.body.userName;
+        registerUser.password = req.body.password;
+        registerUser.email = req.body.email;
+        registerUser.phone = req.body.phone;
+        registerUser.ability= req.body.abiity;        
+        registerUser.age = req.body.age;        
+        registerUser.gender = req.body.gender;
+      registerUser.save(function (err) {
+          if(err){
+              res.json({state : -1, msg : "error is occured"});
+          }
         res.json({ status: 0, message: 'guest register success!' })
       })
-
-      });
-     
-    
+});
 
    
 module.exports = router;

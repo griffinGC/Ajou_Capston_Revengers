e5216be2d0f49b78fc05349b873043344fac5503
griffinGC <griffinDouble@gmail.com>
2019-05-09 21:28:17 +0900

    fix bug

--- farmroad/src/firebase/init.js
+++ farmroad/src/firebase/init.js
@@ -2,7 +2,7 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
 var firebaseConfig = {
-
+df
 };
 // Initialize Firebase
 const firebaseApp =  firebase.initializeApp(firebaseConfig);

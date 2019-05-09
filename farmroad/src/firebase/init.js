b684bd04be3f605eff60d9475aa43eacdb150649
LIJIPU <765149929@qq.com>
2019-05-09 18:21:09 +0900

    delete google firestore API key

--- farmroad/src/firebase/init.js
+++ farmroad/src/firebase/init.js
@@ -2,13 +2,7 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBzzQDHZI75GxN4rYJtiXF3BhEX7EZTI4A",
-  authDomain: "farmroad-chat.firebaseapp.com",
-  databaseURL: "https://farmroad-chat.firebaseio.com",
-  projectId: "farmroad-chat",
-  storageBucket: "farmroad-chat.appspot.com",
-  messagingSenderId: "569763400968",
-  appId: "1:569763400968:web:171d5cd3c014486e"
+ 
 };
 // Initialize Firebase
 const firebaseApp =  firebase.initializeApp(firebaseConfig);

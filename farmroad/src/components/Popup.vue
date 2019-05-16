4a19ab4892cea41df665ce2b920b3c11d0c8aed0
LIJIPU <765149929@qq.com>
2019-05-16 19:24:47 +0900

    added create board function

--- farmroad/src/components/Popup.vue
+++ farmroad/src/components/Popup.vue
@@ -1,6 +1,5 @@
 <template>
   <v-dialog max-width="600px">
-
     <v-btn flat slot="activator" class="success">add host Board</v-btn>
     <v-card>
       <v-card-title>
@@ -37,9 +36,10 @@
           </v-menu>
 
           <v-text-field label="Location" v-model="title" prepend-icon="map"></v-text-field>
+           <v-select :items="items" v-model="diff" label="difficulty" return-object></v-select>
           <v-textarea label="Content" v-model="content" prepend-icon="edit"></v-textarea>
 
-          <v-btn flat class="success mx-0 mt-3">Add project</v-btn>
+          <v-btn flat class="success mx-0 mt-3" @click="createHostBoard">Add Board</v-btn>
         </v-form>
       </v-card-text>
     </v-card>
@@ -47,39 +47,56 @@
 </template>
 <script>
 export default {
- data: vm => ({
+  data() {
+    return {
       date: new Date().toISOString().substr(0, 10),
-      dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
+      dateFormatted: this.formatDate(new Date().toISOString().substr(0, 10)),
       menu1: false,
       menu2: false,
-      title: '',
-      content: ''
-    }),
+      title: "",
+      content: "",
+      diff: '',
+      items: [1,2,3,4,5]
+    };
+  },
 
-    computed: {
-      computedDateFormatted () {
-        return this.formatDate(this.date)
-      }
-    },
-
-    watch: {
-      date (val) {
-        this.dateFormatted = this.formatDate(this.date)
-      }
-    },
-     methods: {
-      formatDate (date) {
-        if (!date) return null
-
-        const [year, month, day] = date.split('-')
-        return `${month}/${day}/${year}`
-      },
-      parseDate (date) {
-        if (!date) return null
-
-        const [month, day, year] = date.split('/')
-        return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`
-      }
+  computed: {
+    computedDateFormatted() {
+      return this.formatDate(this.date);
     }
+  },
+
+  watch: {
+    date(val) {
+      this.dateFormatted = this.formatDate(this.date);
+    }
+  },
+  methods: {
+    formatDate(date) {
+      if (!date) return null;
+
+      const [year, month, day] = date.split("-");
+      return `${month}/${day}/${year}`;
+    },
+    parseDate(date) {
+      if (!date) return null;
+
+      const [month, day, year] = date.split("/");
+      return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
+    },
+    createHostBoard() {
+      this.axios.post("http://ec2-15-164-103-237.ap-northeast-2.compute.amazonaws.com:3000/board/hostCreateBoard", {
+        title: this.title,
+        content: this.content,
+        hostId: localStorage.username,
+        difficulty: this.diff,
+        workDay: 3,
+        startDate: 20190304,
+        endDate: 20190309
+      }).then(resposne=>{
+        console.log(resposne.data)
+      })
+    }
+  }
 };
 </script>

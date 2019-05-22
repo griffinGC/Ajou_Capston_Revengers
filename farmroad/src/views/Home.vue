59b5a4db3d496fe6494aa2ae10256f173bde8678
griffinGC <griffinDouble@gmail.com>
2019-05-22 15:29:54 +0900

    implement main notification

--- farmroad/src/views/Home.vue
+++ farmroad/src/views/Home.vue
@@ -3,21 +3,25 @@
     <h1 class="subheading grey--text">Message</h1>
     <v-container row class="mb-3">
       <!--卡片内容-->
-      <v-card class="pa-3" v-for="project in projects" :key="project.id">
-        <v-layout row wrap :class="`pa-3 project.${project.title}`">
+      <!-- <v-card class="pa-3" v-for="project in projects" :key="project.id"> -->
+      <v-card class="pa-3" v-for="notification in notificationList" :key="notification.id">
+        <v-layout row wrap :class="`pa-3 project.${notification.boardInfo.title}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">title</div>
-            <div>{{project.title}}</div>
+            <!-- <div>{{project.title}}</div> -->
+            <div>{{notification.boardInfo.title}}</div>
           </v-flex>
 
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">preson</div>
-            <div>{{project.guestInfo}}</div>
+            <!-- <div>{{project.guestInfo}}</div> -->
+            <div>{{notification.userName}}</div>
           </v-flex>
 
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Dua by</div>
-            <div>{{project.due}}</div>
+            <!-- <div>{{project.due}}</div> -->
+            <div></div>
           </v-flex>
 
           <v-flex xs6 sm4 md2>
@@ -70,8 +74,45 @@ export default {
           content:
             "尝试Vue.js 最简单的方法是使用JSFiddle 上的Hello World 例子。你可以在浏览器新标签页中打开它，跟着例子学习一些基础用法。或者你也可以创建一个 .html 文件，"
         }
-      ]
+      ],
+      notificationList : ""
     };
+  },
+  created(){
+    console.log("home is created!");
+    this.role = localStorage.role;
+    this.getNotification();
+  },
+  methods:{
+    getNotification(){
+      let userId = localStorage.username;
+      // localStorage.role == 0 이면 guest && 1이면 host
+      // console.log("로컬 스토리지 역할 정보 : "+localStorage.role);
+        if(localStorage.role === '0'){
+        this.axios
+          .get(`http://ec2-15-164-103-237.ap-northeast-2.compute.amazonaws.com:3000/main/guest/getNotification/${userId}`)
+          .then(response => {
+            console.log("응답 값 : " + JSON.stringify(response));
+            // console.log(response.data[0]);
+            this.notificationList = response.data;
+            // console.log(this.notificationList);
+            // console.log("notification list 값 : " + this.notificationList);
+            // console.log(response.data[0].boardInfo.title);
+            // console.log(response.data[0].boardInfo.content);
+            // console.log(response.data[0].userName);
+          });
+      }else{
+        this.axios
+        .get(`http://ec2-15-164-103-237.ap-northeast-2.compute.amazonaws.com:3000/main/host/getNotification/${userId}`)
+        .then(response =>{
+          console.log(response);
+            console.log("응답 값 : " + JSON.stringify(response));
+            // console.log(response.data[0]);
+            this.notificationList = response.data;
+
+        });
+      };
+    },
   },
   components: {}
 };

1cf99ac55935a7c3d071001a2df856f2de5a99ab
Jung June Pyo <forjune@ajou.ac.kr>
2019-05-23 20:16:56 +0900

    modify notifystate

--- backend/routes/notifyState.js
+++ backend/routes/notifyState.js
@@ -5,24 +5,38 @@ var router = express.Router();
 const notifyApproveGuest =require('../schemas/notificationGuest');
 const notifyRefuseGuest =require('../schemas/notificationGuest');
 
+const notifyApproveHost =require('../schemas/notificationHost');
+const notifyRefuseHost =require('../schemas/notificationHost');
 
-router.post('/notifyStateGuest',function(req, res,next){
-  notifyApproveGuest.update({userName : req.body.userName},{$set : {state : approve}},function(err,guestBoardContent){
-      if(req.body.state == approve) {
-        return res.json({state : 0, msg : "Registeration Accept"});
-      };     
+router.post('/notifyApproveStateGuest',function(req, res,next){
+  notifyApproveGuest.update({userName : req.body.userName},{$set : {state : "approve"}},function(err){
+    if(err) {
+      return res.json(err);
+    };
+    //json형식으로 응답
+   
+    return res.json({state : 0, msg : "Registeration approve"});
+         
      
       })
 });  
 
-router.post('/notifyStateGuest',function(req, res,next){
-  notifyRefuseGuest.update({userName : req.body.userName},{$set : {state : refuse}},function(err,guestBoardContent){
-      if(req.body.state == refuse) {
-        return res.json({state : -1, msg : "Registeration Refuse"});
-      };     
+
+      
+router.post('/notifyApproveStateHost',function(req, res,next){
+  notifyApproveHost.update({userName : req.body.userName},{$set : {state : "approve"}},function(err){
+    if(err) {
+      return res.json(err);
+    };
+    //json형식으로 응답
+   
+    return res.json({state : 0, msg : "Registeration approve"});
+         
      
       })
-});
+});  
+
+
       
    
 module.exports = router;

a3d2bc54339d27b8417a539268be9960e83e5f5d
LIJIPU <765149929@qq.com>
2019-05-29 23:15:42 +0900

    added fix profile img function

--- farmroad/src/views/MyBoards.vue
+++ farmroad/src/views/MyBoards.vue
@@ -153,7 +153,31 @@ export default {
             }
           });
       }
-    }
+    },
+    
+    pickFile() {
+      this.$refs.image.click();
+    },
+
+    onFilePicked(e) {
+      const files = e.target.files;
+      if (files[0] !== undefined) {
+        this.imageName = files[0].name;
+        if (this.imageName.lastIndexOf(".") <= 0) {
+          return;
+        }
+        const fr = new FileReader();
+        fr.readAsDataURL(files[0]);
+        fr.addEventListener("load", () => {
+          this.imageUrl = fr.result;
+          this.imageFile = files[0]; // this is an image file that can be sent to server...
+        });
+      } else {
+        this.imageName = "";
+        this.imageFile = "";
+        this.imageUrl = "";
+      }
+    },
   }
 };
 </script>

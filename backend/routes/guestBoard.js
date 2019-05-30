b685f943cdb8211c58344d7b9758a6091577dfed
griffinGC <griffinDouble@gmail.com>
2019-05-30 19:32:57 +0900

    implement admin boardList

--- backend/routes/guestBoard.js
+++ backend/routes/guestBoard.js
@@ -24,6 +24,17 @@ router.get('/getList',function(req, res,next){
         })
   });        
 
+  //금지된 host정보 가져오기
+ router.get('/getBan', function(req, res, next){
+    guestBoard.find({report : true}, function(err, getInfo){
+        if(err){
+            return res.json(err);
+        };
+        return res.json(getInfo);
+    });
+}) 
+
+
 //guest게시판 작성자 정보 가져오기
 router.get('/getMsg/:id', function(req, res, next){
     guestModel.find({userName : req.params.id}, function(err, guestIdInfo){
@@ -34,11 +45,25 @@ router.get('/getMsg/:id', function(req, res, next){
     });
 })
 
-//board post test
-router.post('/', function(req, res, next){
-    res.json({state : "testNormal"});
-});  
 
+//hostBoard 게시글 금지시키기 
+router.get('/updateGuest/ban/:id', function(req, res, next){
+    guestBoard.update({boardId : req.params.id}, {$set : {report : true}}, function(err){
+        if(err){
+            return res.json(err);
+        };
+        return res.json({state : 0, msg : "update guestBoard ban is success"});
+    });
+})
+//hostBoard 게시글 금지 해제 시키기 
+router.get('/updateGuest/able/:id', function(req, res, next){
+    guestBoard.update({boardId : req.params.id}, {$set : {report : false}}, function(err){
+        if(err){
+            return res.json(err);
+        };
+        return res.json({state : 0, msg : "update guestBoard able is success"});
+    });
+})
 
 var newFile = "";
 

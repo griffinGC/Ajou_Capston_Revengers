5ce2dc2bfce7029df8348ffc9da9cbbbbcf849d1
LIJIPU <765149929@qq.com>
2019-05-30 13:43:19 +0900

    complete img upload function in edituserinfo.vue

--- farmroad/src/components/Popup.vue
+++ farmroad/src/components/Popup.vue
@@ -116,7 +116,7 @@ export default {
       const [month, day, year] = date.split("/");
       return `${year}-${month.padStart(2, "0")}-${day.padStart(2, "0")}`;
     },
-     pickFile() {
+    pickFile() {
       this.$refs.image.click();
     },
 

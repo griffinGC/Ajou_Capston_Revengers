51408cdd9a7562a8a7486b7fca445d895874119e
griffinGC <griffinDouble@gmail.com>
2019-06-06 18:59:16 +0900

    implement get reference Info backend

--- backend/routes/saveReference.js
+++ backend/routes/saveReference.js
@@ -39,10 +39,59 @@ router.post('/createHostReference',function(req, res, next){
         }
         res.json({state : 0, msg : "write reference for host is success"});
     })
-
-
 })
 
+//host => guest 후기 글 작성 
+router.post('/createGuestReference',function(req, res, next){
+    if(!req.body.title){
+        return res.json({state : -1, msg : "reference Title is empty!"});
+    }
+    if(!req.body.content){
+        return res.json({state : -1, msg : "reference Content is empty!"});
+    }
+
+    let writeReference = new referenceModel();
+
+    writeReference.boardId = req.body.boardId;
+    //작성되는곳 host profile
+    writeReference.boardType = "guest";
+    //새로 작성될 title
+    writeReference.title = req.body.title;
+    writeReference.writer = req.body.writer;
+    //작성될 guest profile
+    writeReference.userName = req.body.uerName;
+    let day = new Date();
+    let yy = day.getFullYear();
+    let mm = day.getMonth()
+    let dd = day.getDate();
+    writeReference.writeDay = yy + '-' + mm + '-'+ dd;
+    writeReference.content = req.body.content;
+    writeReference.star = req.body.star;
+
+    writeReference.save(function(err){
+        if(err){
+            console.log(err);
+            return res.json({state : -1, msg : "write reference for host is failed"});
+        }
+        res.json({state : 0, msg : "write reference for host is success"});
+    })
+})
+
+//host가 자기한테 작성된 reference가져오기 
+router.get('/getHostReference/:id',function(req, res,next){
+    console.log(req.params.id);
+    if(!req.params.id){
+      return res.json({state : -1, msg : "host ID is empty"});
+    }
+      referenceModel.find({userName : req.params.id, boardType : "host"},function(err,getInfo){
+          if(err) {
+            return res.json(err);
+          };
+          if(getInfo){
+              return res.json(getInfo)
+          } 
+        }); 
+  });
 
 
    

53d9e6de8aaabf9b096a888af10ea802aa633908
LIJIPU <765149929@qq.com>
2019-06-12 21:08:28 +0900

    added GMap page

---
+++ farmroad/src/views/GMap.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="map">
+    <h2>Map</h2>
+    <div class="google-map" id="map"></div>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+import { setTimeout } from 'timers';
+export default {
+  name: "GMap",
+  data() {
+    return {
+      lat: 53,
+      lng: -2
+    };
+  },
+  methods: {
+    renderMap() {
+      const map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: this.lat, lng: this.lng },
+        zoom: 6,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false
+      });
+    }
+  },
+  mounted() {
+    this.renderMap();
+    console.log(firebase.auth().currentUser)
+  }
+};
+</script>
+
+<style>
+.map {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+}
+.google-map{
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  background-attachment: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
+</style>
